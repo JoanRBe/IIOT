975418554477a97cbb19c2918fa31e463625dcb1
2001judit <1585504@campus.euss.org>
2024-11-04 20:26:32 +0100

    updated SPR3

---
+++ libs/email/email.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "email.h"
+void email(char *arg1, char *arg2, char *arg3, char *arg4){
+printf("Val1=%s\n", arg1);
+printf("Val2=%s\n", arg2);
+printf("Val3=%s\n", arg3);
+printf("Val4=%s\n", arg4);
+}
+
---
+++ libs/email/email.h
@@ -0,0 +1,4 @@
+#ifndef FUNC_H
+#define FUNC_H
+void email(char *arg1, char *arg2, char *arg3, char *arg4);
+#endif
